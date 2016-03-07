5d6c6458e5f4d9dd30e82b5fe0d9715435dee88d
David Ellsworth <dells84@gmail.com>
2016-03-07 15:24:32 -0700

    cleanup firefox droppings

--- startup.sh
+++ startup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+export DISPLAY=:99
 
 while true; do
-    python pucauto.py
-    echo $(date +"%D %D") "Restarting..."
-    killall firefox
+    python pucauto_de.py
+    echo $(date +"%D %T") "Restarting..."
     killall firefox.real
+    #killall firefox
+    rm -rf /tmp/tmp*
     sleep 3
 done
